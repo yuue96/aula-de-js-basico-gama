d66a9d5b775deea2bb90c14fedb0ad7d72afa6d8
Yuna <yunauehara32@gmail.com>
2021-07-08 21:55:41 -0300

    Arquivos adicionados

---
+++ script.js
@@ -0,0 +1,76 @@
+console.log("Hello World!")
+
+// 2. Variáveis var, let e const
+// var - pode ocorrer hoisting
+// let - recomendado usá-lo
+// const - não podem ser atualizadas nem reatribuídas
+
+var myName="Yu"
+console.log(myName)
+
+const myLastName="Ue"
+console.log(myLastName)
+
+
+// 3. Tipos de dados
+// string, number, null, undefined, boolean
+// objeto, array e function
+
+
+function sum(){
+    return a+b 
+}
+
+console.log(typeof sum)
+
+
+// Operadores
+
+//Operadores aritméticos
+
+var sumTwoNumbers = 1+3
+console.log(sumTwoNumbers)
+
+var subTwoNumers = 4-3
+console.log(subTwoNumers)
+
+var multTwoNumbers = 4*3
+console.log(multTwoNumbers)
+
+var divTwoNumbers = 4/2
+console.log(divTwoNumbers)
+
+console.log(8%3)
+
+var num = 1
+console.log(++num)
+console.log(--num)
+
+
+// Operadores de atribuição
+// = atribuição
+
+// atribuição de soma
+
+var x = 1
+var y = 3
+console.log(x+=y) // == x = x+y)
+console.log(x-=y)
+console.log(x*=y)
+console.log(x/=y)
+console.log(x%=y)
+
+
+// Operadores de comparação
+// == Comparação de valores
+//=== Valor e tipo
+// != Diferente de 
+// < ou <=
+// > ou >=
+
+
+// Operadores lógicos (verificações)
+
+// e &&
+// ou ||
+// ! (negação)
