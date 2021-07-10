1b47ed2dfeb32938ccf1126b770f202d20272c52
Yuna <yunauehara32@gmail.com>
2021-07-10 20:36:11 -0300

    Conteúdo da aula

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Hello World!")
 
-// Variáveis var, let e const
+// 2. Variáveis var, let e const
 // var - pode ocorrer hoisting
 // let - recomendado usá-lo
 // const - não podem ser atualizadas nem reatribuídas
@@ -13,7 +13,7 @@ console.log(myLastName)
 
 
 
-// Tipos de dados
+// 3. Tipos de dados
 // string, number, null, undefined, boolean
 // objeto, array e function
 
@@ -26,7 +26,7 @@ console.log(typeof sum)
 
 
 
-// Operadores
+// 4. Operadores
 
 //Operadores aritméticos
 
@@ -79,7 +79,7 @@ console.log(x%=y)
 
 
 
-// Condicionais if, if else e switch
+// 5. Condicionais if, if else e switch
 
 var num = 1;
 num =  5;
@@ -118,7 +118,7 @@ switch (mes) {
 }
 
 
-// Estrutura de Repetição/Laço/Loop for while e do while
+// 6. Estrutura de Repetição/Laço/Loop for while e do while
 // arrays
 // contagem começa do 0 []
 
@@ -154,5 +154,94 @@ do {
     console.log(i);
     i++;
 } while (i < 10)
+
+
+
+// 7. Funções 
+
+function sayHello() {
+    console.log("Hello")
+}
+
+sayHello();
+
+function sayHelloo(name, lastName) {
+    console.log("Helloo " + name + ' ' + lastName)
+}
+
+sayHelloo("Yu", "Ue");
+
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(40,60));
+
+
+
+// 8. Classes (objeto)
+
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    read() {
+        return `Estou lendo  ${this.title} `
+    }
+}
+
+let book = new Book("Algoritmos para viver", "Brian", 500);
+let otherBook = new Book("Um Exu em Nova York", "Cidinha da Silva", 100);
+
+console.log(book)
+console.log(book.read())
+console.log(otherBook)
+console.log(otherBook.read())
+console.log(book, otherBook)
+
+
+// Herança (herdar as propriedades da classe Book)
+
+class ITBook extends Book {
+    constructor(title,author, pages,technology) {
+        super(title, author, pages);
+        this.technology = technology;
+    }
+}
+
+let itBook = new ITBook("Algoritmos para viver", "Brian", 500, "Algoritmos");
+
+console.log(itBook.title);
+console.log(itBook);
+
+
+// encapsulamento (getters e setters)
+
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+}
+
+
+let person = new Person("Yu");
+person.name = "Yuyu"
+console.log(person.name)
+
+
+
 
 
